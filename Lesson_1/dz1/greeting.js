ff0d1e8759559b596a16b710b2a83211a8189ee1
Aiymkan <aiymkan@Aiymkans-MacBook-Pro.local>
2022-07-12 20:25:25 +0600

    dz1

---
+++ Lesson_1/dz1/greeting.js
@@ -0,0 +1,55 @@
+const name = prompt("Your name")
+console.log(name)
+alert(`Hello! ${name}`);
+
+
+
+var x = "color"
+    switch(x){
+        case 1:  alert("red");
+        break;
+        case 2:  alert("yellow");
+        break;
+        case 3:  alert("green");
+        break;
+
+}
+
+
+
+var prd=2,                 /* время показа сигналов в секундах */
+    ylz=10,                /* количество повторений желтого */
+    ylz_speed=0.25,        /* скорость отсчета желтого в секундах */
+    to='',                 /* таймаут */
+    lights=[],             /* дивы */
+    dr='dwn';              /* направление (какое направление? очень плохая музыка!) */
+
+function trafficlite(n){
+/* на всякий случай */
+clearTimeout(to);
+var n=arguments[0]||0,
+    y=arguments[1]||0;
+
+[].forEach.call(lights,function(k,i){k.className=i!=n?'blk':'';});
+
+switch(n){
+case 0:
+case 2:  n < 1?(n++,dr='dwn'):(n--,dr='up');
+         to=setTimeout('trafficlite('+n+')',prd*1000);
+         break;
+case 1:  to=setTimeout(
+         function(){
+         y < ylz ?
+         (trafficlite(n,y+1),lights[n].innerHTML=y+1)
+         :
+         (lights[n].innerHTML='',dr=='dwn'?n++:n--,trafficlite(n))
+         },ylz_speed*1000);
+         break;
+default:;
+}
+}
+
+onload=function(){
+lights=document.querySelectorAll('div');
+trafficlite();
+}
---
+++ lesson2/app.js
@@ -0,0 +1,79 @@
+// const array = [
+//     {
+//         username: 'Jack',
+//         full_name: 'Jack Barbaro'
+//     },
+//     {
+//         username: 'Jack',
+//         full_name: 'Jack Barbaro'
+//     },
+//     {
+//         username: 'Jack',
+//         full_name: 'Jack Barbaro'
+//     },
+//     {
+//         username: 'Jack',
+//         full_name: 'Jack Barbaro'
+//     },
+//     {
+//         username: 'Jack',
+//         full_name: 'Jack Barbaro'
+//     },
+// ]
+
+// console.log(array[0].username, array[0].full_name)
+// for(let i=0; i < array.length; i++){
+//     console.log(array[i]) //индексы
+//
+// }
+// for(let user of array){
+//     console.log(user) //элементы
+// }
+// const obj = {
+//     key1:'b1',
+//     key2:'b2',
+//     key3:'b3',
+//     key4:'b4',
+//     key5:'b5',
+//     key6:'b6',
+//     key7:'b7',
+// }
+// for(let key in obj){
+//     console.log(obj[key])
+// }
+
+// let i = 0
+//
+// while(i < 5){
+//     console.log("Hello")
+//     i++
+// }
+
+
+// Треугольник в цикле
+var str = "*";
+
+while (str.length<=7){
+
+    console.log(str);
+
+    console.log();
+
+    str+="*";
+
+}
+// FizzBuzz
+
+const list = [];
+for (let i = 1; i <= 100; i++) {
+if (i % 15 === 0) {
+list.push("FizzBuzz");
+} else if (i % 3 === 0) {
+list.push("Fizz");
+} else if (i % 5 === 0) {
+list.push("Buzz");
+} else {
+list.push(i);
+}
+}
+console.log(list);
